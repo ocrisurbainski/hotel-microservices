a2edfb402ed2b7cc48cb46093438a642d155f846
ocrisurbainski <Cristian Urbainski>
2022-07-14 10:22:51 -0300

    Removido classe de property que não é mais necessária ao projeto.

--- src/main/java/com/urbainski/reservasapi/Application.java
+++ src/main/java/com/urbainski/reservasapi/Application.java
@@ -2,12 +2,8 @@ package com.urbainski.reservasapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.urbainski.reservasapi.config.ReservasApiProperty;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ReservasApiProperty.class)
 public class Application {
 
 	public static void main(String[] args) {
