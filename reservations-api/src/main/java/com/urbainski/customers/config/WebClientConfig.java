5f5c2e048bb2d1eb2c4a029cfb06970804c953a2
ocrisurbainski <Cristian Urbainski>
2022-08-07 16:20:50 -0300

    Adicionado o retry para caso aconteça erro na chamada seja feita nova tentativa, configurado até 3 tentativas e com delay de 5 segundos entre cada tentativa.

--- reservations-api/src/main/java/com/urbainski/customers/config/WebClientConfig.java
+++ reservations-api/src/main/java/com/urbainski/customers/config/WebClientConfig.java
@@ -12,9 +12,12 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
+import reactor.util.retry.Retry;
 
+import java.net.ConnectException;
 import java.time.Duration;
 
 @Slf4j
@@ -41,7 +44,7 @@ public class WebClientConfig {
     public ReactorClientHttpConnector reactorClientHttpConnector(
             @Value("${clients.config.timeout.seconds}") int timeoutConfigured) {
 
-        log.debug("TIMEOUT confugado com: {}  segundos", timeoutConfigured);
+        log.debug("TIMEOUT configured with: {} seconds", timeoutConfigured);
 
         final var secondsTimeout = Duration.ofSeconds(timeoutConfigured);
         final var httpClient = HttpClient.create()
@@ -54,12 +57,38 @@ public class WebClientConfig {
         return new ReactorClientHttpConnector(httpClient.wiretap(true));
     }
 
+    @Bean
+    public Retry retry(
+            @Value("${clients.config.retry.max-attempts}") Integer retryMaxAttempts,
+            @Value("${clients.config.retry.fixed-delay-in-seconds}") Integer retryFixedDelayInSeconds) {
+
+        log.debug("RETRY configured with: {} number of attempts and with fixed delay: {} seconds", retryMaxAttempts, retryFixedDelayInSeconds);
+
+        return Retry
+                .fixedDelay(retryMaxAttempts, Duration.ofSeconds(retryFixedDelayInSeconds))
+                .filter(this::isRetry)
+                .doAfterRetry(retrySignal -> log.info(retrySignal.toString()))
+                .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) -> retrySignal.failure());
+    }
+
     private static ExchangeFilterFunction logRequestFilter() {
         return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
             log.info("Request: {} {}", clientRequest.method(), clientRequest.url());
             clientRequest.headers().forEach((name, values) -> values.forEach(value -> log.info("{}={}", name, value)));
             return Mono.just(clientRequest);
         });
+    }
+
+    private boolean isRetry(Throwable throwable) {
+        if (throwable instanceof ConnectException || throwable.getCause() instanceof ConnectException) {
+            return true;
+        }
+
+        if (throwable instanceof WebClientResponseException) {
+            return ((WebClientResponseException) throwable).getStatusCode().is5xxServerError();
+        }
+
+        return false;
     }
 
 }
--- reservations-api/src/main/java/com/urbainski/customers/webclient/CustomerWebClientComponent.java
+++ reservations-api/src/main/java/com/urbainski/customers/webclient/CustomerWebClientComponent.java
@@ -6,15 +6,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
 @Slf4j
 @Component
 public class CustomerWebClientComponent {
 
     private final WebClient webClient;
+    private final Retry retry;
 
-    public CustomerWebClientComponent(@Qualifier("customers") WebClient webClient) {
+    public CustomerWebClientComponent(
+            @Qualifier("customers") WebClient webClient, Retry retry) {
         this.webClient = webClient;
+        this.retry = retry;
     }
 
     public Mono<Boolean> checkCustomerExists(String customerId) {
@@ -26,6 +30,7 @@ public class CustomerWebClientComponent {
                     }
                     return Mono.just(Boolean.FALSE);
                 })
+                .retryWhen(retry)
                 .doOnSubscribe(s -> log.debug("Verify the customer with identifier: {} exists.", customerId))
                 .doOnSuccess(response -> log.debug("Success verify that the customer with the identifier: {} exists.", customerId))
                 .doOnError(e -> log.debug("Error verify that the customer with the identifier: {} exists.", customerId));
